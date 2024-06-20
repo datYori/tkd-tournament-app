a5dee2adaff6776970b0c9aeac6ad4daac8e21bd
yann <yann@raw-labs.com>
2024-06-21 00:16:02 +0200

    cope with new mongo image auth mode

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 existing_container="$(docker ps -aq --filter "name=.*tournament*")"
 
 if [ -z "$existing_container" ]; then
-  docker run --name tournament-mongo -p 27017:27017 -d mongo
+  docker run --name tournament-mongo -p 27017:27017 -d mongo --noauth
 else
   docker start $existing_container
 fi
